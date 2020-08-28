411064a93169c4d745caa4f7aa5d216bda022f6e
Joe A. Spandrusyszyn <jspandrusyszyn@rocsoft.com>
2020-08-28 17:46:14 -0400

    Add prettier config and fix code style; fixes #2

--- init.js
+++ init.js
@@ -1,16 +1,16 @@
-Hooks.on("canvasReady", canvas => {
-  canvas.tokens.placeables.forEach(token => {
-    token.effects.addListener("childAdded", child => {
+Hooks.on('canvasReady', (canvas) => {
+  canvas.tokens.placeables.forEach((token) => {
+    token.effects.addListener('childAdded', (child) => {
       fixEffectScale(token, child);
     });
     updateEffectScales(token);
   });
 });
 
-Hooks.on("createToken", (parent, tokenData, options, userId) => {
+Hooks.on('createToken', (parent, tokenData, options, userId) => {
   setTimeout(() => {
     const token = canvas.tokens.get(tokenData._id);
-    token.effects.addListener("childAdded", child => {
+    token.effects.addListener('childAdded', (child) => {
       fixEffectScale(token, child);
     });
     updateEffectScales(token);
@@ -29,22 +29,28 @@ function fixEffectScale(token, child) {
 function updateEffectScales(token) {
   const numEffects = token.data.effects.length;
   if (numEffects > 0) {
-    const w = Math.floor(token.width / 3)-1;
+    const w = Math.floor(token.width / 3) - 1;
     const bg = token.effects.children[0];
     bg.clear();
-    bg.beginFill(0x000000, 0.60).lineStyle(1.0, 0x000000);
+    bg.beginFill(0x000000, 0.6).lineStyle(1.0, 0x000000);
     token.effects.children.forEach((effectIcon, i) => {
-      if ( i === 0 ) {
+      if (i === 0) {
         // BG
-      } else if ( i <= numEffects ) {
+      } else if (i <= numEffects) {
         // Effect icon
-        const ei = i-1;
+        const ei = i - 1;
         const x = (ei % 3) * w;
         const y = Math.floor(ei / 3) * w;
         effectIcon.width = effectIcon.height = w;
         effectIcon.position.x = x;
         effectIcon.position.y = y;
-        bg.drawRoundedRect(effectIcon.position.x, effectIcon.position.y, effectIcon.width, effectIcon.width, 2);
+        bg.drawRoundedRect(
+          effectIcon.position.x,
+          effectIcon.position.y,
+          effectIcon.width,
+          effectIcon.width,
+          2
+        );
       } else {
         // Overlay icon
       }
